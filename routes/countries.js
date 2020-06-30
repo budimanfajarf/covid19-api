d34ff1a16ca358750d8e731aeadd824118b0a2ab
Budiman Fajar Firdaus <budimanfajarf@gmail.com>
2020-06-30 15:38:17 +0700

    Update countries.js
    Edited online in GitHub web-mobile

--- routes/countries.js
+++ routes/countries.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getCountries, getDegetDetailCountry } = require('../ext-api/covid19api.com/main');
+const { getCountries, getCountry } = require('../ext-api/covid19api.com/main');
 
 router.get('/', async (req, res, next) => {
   const countries = await getCountries();
@@ -11,8 +11,8 @@ router.get('/:slug', async (req, res, next) => {
   const slug = req.params.slug;
   const from = req.query.from || false;
   const to = req.query.to || false;
-  const specifyCountry = await getDetailCountry(slug, from, to);
-  res.json(specifyCountry);
+  const country = await getCountry(slug, from, to);
+  res.json(country);
 }); 
 
 module.exports = router;
