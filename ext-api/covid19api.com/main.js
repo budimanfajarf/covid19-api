d8ab8e549c5e4bd323dcc62ff7e5849be52982f9
Budiman Fajar Firdaus <budimanfajarf@gmail.com>
2020-06-30 15:13:31 +0700

    Update main.js

--- ext-api/covid19api.com/main.js
+++ ext-api/covid19api.com/main.js
@@ -1,15 +1,15 @@
-const { apiSummaryWithGlobalDate, apiSummary, apiDayOneAllStatus, apiByCountryAllStatus } = require('./default');
-const { generateGlobal, generateCountries, generateSpecifyCountry, generateDetailCountry } = require('./function');
+const { apiSummary, apiSummaryWithGlobalDate, apiDayOneAllStatus, apiByCountryAllStatus } = require('./default');
+const { remapGlobal, remapCountries, remapCountry } = require('./function');
 
 getSummary = () => {
   return new Promise((resolve, reject) => {
     apiSummaryWithGlobalDate()
     .then((data) => {
-      const newGlobal = generateGlobal(data.Global);
-      const newCountries = generateCountries(data.Countries);
+      const global = remapGlobal(data.Global);
+      const countries = remapCountries(data.Countries);
       resolve({
-        global: newGlobal,
-        countries: newCountries
+        global,
+        countries
       })
     })
     .catch((err) => reject(err));    
@@ -20,8 +20,8 @@ getGlobal = () => {
   return new Promise((resolve, reject) => {
     apiSummaryWithGlobalDate()
     .then((data) => {
-      const newGlobal = generateGlobal(data.Global);
-      resolve(newGlobal);       
+      const global = remapGlobal(data.Global);
+      resolve(global);       
     })
     .catch((err) => reject(err));    
   });
@@ -31,29 +31,29 @@ getCountries = () => {
   return new Promise((resolve, reject) => {
     apiSummary()
     .then((data) => {
-      const newCountries = generateCountries(data.Countries);
-      resolve(newCountries);       
+      const countries = remapCountries(data.Countries);
+      resolve(countries);       
     })
     .catch((err) => reject(err));     
   });
 }
 
-getDetailCountry = (slug, from, to) => {
+getCountry = (slug, from, to) => {
   return new Promise((resolve, reject) => {
-    let specifyCountry;
+    let country;
     if (from && to) {
       apiByCountryAllStatus(slug, from, to)
       .then((data) => {
-        specifyCountry = generateDetailCountry(data, slug);
-        resolve(specifyCountry);       
+        country = remapCountry(data, slug);
+        resolve(country);       
       })
       .catch((err) => reject(err));       
     }
     else {
       apiDayOneAllStatus(slug)
       .then((data) => {
-        specifyCountry = generateDetailCountry(data, slug);
-        resolve(specifyCountry);       
+        country = remapCountry(data, slug);
+        resolve(country);       
       })
       .catch((err) => reject(err));            
     } 
@@ -63,4 +63,4 @@ getDetailCountry = (slug, from, to) => {
 module.exports.getSummary = getSummary;
 module.exports.getGlobal = getGlobal;
 module.exports.getCountries = getCountries;
-module.exports.getDetailCountry = getDetailCountry;
+module.exports.getCountry = getCountry;
